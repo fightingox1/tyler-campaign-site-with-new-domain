ec0b9ed669649c43fb1dc1b97d240a2cbf8e1be2
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-09-12 14:12:15 -0400

    Add files via upload

---
+++ votinginterface.py
@@ -0,0 +1,10 @@
+f = "1"
+while f == "1":
+  a = input("click y for voting for me type n if your not")
+  if a == "y":
+    print("thankyou for voting for tyler")
+  if a == "y":
+    print("thank you for taing the poll we hope we can change your mind on the decision")
+  else:
+    print("an error occord when trying to register your vote please check that caps lock is off and ty again")
+    f = "1"
